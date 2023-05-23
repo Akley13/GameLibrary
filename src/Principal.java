943c746faf20f23bef967c5357d80e15dc1305d4
Akley Alves <akleyalvescr@gmail.com>
2023-05-23 17:02:20 -0300

    updt

--- src/Principal.java
+++ src/Principal.java
@@ -3,5 +3,6 @@ public class Principal {
         Games gamePreferido = new Games();
         gamePreferido.nome = "The last of us";
         gamePreferido.anoDeLancamento = 2013;
+        gamePreferido.historiaDoJogo = "A história de survival horror se passa em um mundo pós-apocalíptico. O fungo cordyceps infecta pessoas e criaturas e as torna “zumbis”. O desenrolar acontece num futuro distópico que começa com o surto da doença ocorrendo em 2013, mas o jogo em si se passa vinte anos depois, em 2033.";
     }
 }
