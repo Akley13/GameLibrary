a5039486c6771b576e6d08e01534f2c21fb57dc5
Akley Alves <akleyalvescr@gmail.com>
2023-05-23 16:31:37 -0300

    Initial commit

---
+++ src/Games.java
@@ -0,0 +1,10 @@
+public class Games {
+    String nome;
+    String historiaDoJogo;
+    String desenvolveraDoJogo;
+    int anoDeLancamento;
+    double avaliacaoMetacritic;
+    double avaliacaoPessoal;
+
+
+}
---
+++ src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) {
+        Games gamePreferido = new Games();
+        gamePreferido.nome = "The last of us";
+        gamePreferido.anoDeLancamento = 2013;
+    }
+}
