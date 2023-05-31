6ea2cae4879b653f3a5b2300f7cf7a18141f63ff
Akley Alves <akleyalvescr@gmail.com>
2023-05-31 20:46:07 -0300

    up

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,14 @@
+import javax.sound.midi.Soundbank;
+
 public class Principal {
     public static void main(String[] args) {
+        System.out.println("GAME LIBRARY");
+        System.out.println();
+        System.out.println("Game: The last of us");
+
+        int releaseYear = 2013;
+
+        System.out.println("Release Year: " + releaseYear);
 
     }
 
