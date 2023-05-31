957a08b965bae16746996e25c9257797e21520ed
Akley Alves <akleyalvescr@gmail.com>
2023-05-31 20:33:04 -0300

    updt

--- src/Games.java
+++ src/Games.java
@@ -1,14 +1,4 @@
 public class Games {
-    String nome;
-    String desenvolveraDoJogo;
-    String historiaDoJogo;
-    int anoDeLancamento;
 
-
-    void exibeDadosDoJogo() {
-        System.out.println("Nome do jogo: " + nome);
-        System.out.println("Desenvolvedora: " + desenvolveraDoJogo);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-        System.out.println("História: " + historiaDoJogo);
     }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,11 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
-        Games gamePreferido = new Games();
-        gamePreferido.nome = "The last of us";
-        gamePreferido.desenvolveraDoJogo = "Naughty Dog";
-        gamePreferido.anoDeLancamento = 2013;
-        gamePreferido.historiaDoJogo = "A história de survival horror se passa em um mundo pós-apocalíptico. O fungo cordyceps infecta pessoas e criaturas e as torna “zumbis”. O desenrolar acontece num futuro distópico que começa com o surto da doença ocorrendo em 2013, mas o jogo em si se passa vinte anos depois, em 2033.";
 
-        gamePreferido.exibeDadosDoJogo();
     }
+
 }
