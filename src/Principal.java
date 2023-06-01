f76c99f4a033949c0612e9adbb5bbdc8f692848f
Akley Alves <akleyalvescr@gmail.com>
2023-05-31 21:37:05 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -9,11 +9,17 @@ public class Principal {
         int anoLancamento = 2013;
         System.out.println("Ano de lançamento: " + anoLancamento);
 
+        String historia = "A história de survival horror se passa em um mundo pós-apocalíptico. O fungo cordyceps infecta pessoas e criaturas e as torna “zumbis”. O desenrolar acontece num futuro distópico que começa com o surto da doença ocorrendo em 2013, mas o jogo em si se passa vinte anos depois, em 2033.";
+        System.out.println("História: " + historia);
+
+        double media = (9.5 + 10 + 8) / 3;
+        System.out.println("Média de avaliação: " + media);
+
         double notaDoJogo = 10;
         System.out.println("Nota: " + notaDoJogo);
 
-        boolean incluinoNoPlano = true;
-        System.out.println("Plano: " + incluinoNoPlano);
+        boolean assinatura = true;
+        System.out.println("Plano: " + assinatura);
 
     }
 
